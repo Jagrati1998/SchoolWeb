a08a701f05a1a51c54d4e97596df749795f79286
Jagrati Mishra <jagratimishra2222@gmail.com>
2020-12-19 18:30:11 +0530

    first commit

---
+++ Mypage/Logo.php
@@ -0,0 +1,259 @@
+
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<title>Scholarly an Education Category Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--// Meta tag Keywords -->
+<!-- css files -->
+<link rel="stylesheet" href="../css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="../css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+<link rel="stylesheet" href="../css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<link rel="stylesheet" href="../css/swipebox.css">
+<link rel="stylesheet" href="../css/jquery-ui.css" />
+<!-- //css files -->
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+<!-- //online-fonts -->
+</head>
+<body>
+<!-- banner -->
+<div class="main_section_agile" id="home">
+	<div class="agileits_w3layouts_banner_nav">
+		<nav class="navbar navbar-default">
+			<div class="navbar-header navbar-left">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<h1><a class="navbar-brand" href="index.html"><i class="fa fa-leanpub" aria-hidden="true"></i> scholarly</a></h1>
+
+			</div>
+			<div class="w3layouts_header_right">
+			    <form action="#" method="post">
+					<input name="Search here" type="search" placeholder="Search" required="">
+					<input type="submit" value="">
+				</form>
+			</div>
+			<ul class="agile_forms">
+				<li><a class="active" href="students/index.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Sign In now</a> </li>
+			<!--	<li><a href="#" data-toggle="modal" data-target="#myModal3"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sign Up</a> </li> -->
+			</ul>
+			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+				<nav class="link-effect-2" id="link-effect-2
+				<a id="resp-menu" class="responsive-menu" href=""> Menu</a>
+					<ul class="nav navbar-nav">
+					
+						
+					<li class="active">
+                        <a class="dropdown-toggle" data-toggle="dropdown">About us
+                        </a>
+                        <ul class="dropdown-menu">
+                                        
+                                                <li><a href="Logo.php" target="_self">Logo and Values</a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/vision-mission-2/" target="_self">Vision and Mission </a></li>
+                                                <!--<li><a href="#">Principal's Message</a></li>-->
+                                                <li><a href="http://www.titikshapublicschool.com/dcertification-and-affiliation/" target="_blank">Affiliation</a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/school-espirit/" target="_blank">School Espirit</a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/school-rules-regulations-2/" target="_blank">School Rules and Regulations</a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/school-activity-calendar-2019/" target="_blank">School Activity Calendar</a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/wp-content/uploads/2020/07/FACULTY-PROFILE-20.7.2020.pdf" target="_blank"> Faculty Profile</a></li>
+                                               
+                                
+                                                <li><a href="http://www.titikshapublicschool.com/wp-content/uploads/2020/07/POCSO-COMMITTEE-Member-list.pdf" target="_blank"> POCSO Committee </a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/wp-content/uploads/2020/07/school-disciplinary-committees-1.pdf" target="_blank"> School Disciplinary Committee </a></li>
+                                                <li><a href="http://www.titikshapublicschool.com/wp-content/uploads/2020/07/SCHOOL-SAFETY-COMMITTEE.pdf" target="_blank"> School Safety Committee </a></li>
+
+                                            </ul>
+                                        </li>
+                                                
+					<!-- 	<li><a href="#about" class="effect-3 scroll">About Us</a></li>-->
+						<li><a href="#services" class="effect-3 scroll">Services</a></li>
+						<li><a href="#team" class="effect-3 scroll">Team</a></li>
+						<li><a href="#gallery" class="effect-3 scroll">Gallery</a></li>
+						<li><a href="#mail" class="effect-3 scroll">Mail Us</a></li>
+					</ul>
+				</nav>
+
+			</div>
+		</nav>	
+		<div class="clearfix"> </div> 
+	</div>
+</div>
+<!-- my edit -->
+<div class="topnav" id="Topnav" style="background-color:#A7957D;margin-top: 0px">
+                                        <a href="#about" style="margin-top:5px">| Contact Links : |</a>
+                                        <a href="https://www.facebook.com/TitikshaPublicSchool/" target="_blank">
+                                            <span style="color:#1a0dab">
+                                                <img src="../wp-content\fb.png" style="height:30px" /> <!--<i class="fa fa-facebook" style="margin-top:6px"></i>-->
+                                            </span>
+                                        </a>
+                                        <a href="https://twitter.com/thetitiksha" target="_blank">
+                                            <span style="color:#00ACED">
+                                                <img src="../wp-content\tw.png" style="height:30px" /><!--<i class="fa fa-twitter" style="margin-top:6px"></i>-->
+                                            </span>
+                                        </a>
+                                        <!-- <a href="https://www.instagram.com/titiksha_public_school/" target="_blank">
+                                           <span style="color:#003569">
+                                              <img src="data1/img/insta.png" style="height:30px" /><i class="fa fa-instagram" style="margin-top:6px"></i>
+                                           </span>
+                                        </a>-->
+                                        <a href="https://www.youtube.com/user/Titiksha11" target="_blank">
+                                            <span style="color:#FD2725">
+                                                <img src="../wp-content/yt.png" style="height:30px" /> <!--<i class="fa fa-youtube" style="margin-top:6px"></i>-->
+                                            </span>
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/titiksha-public-school-85a9b4175" target="_blank">
+                                            <span style="color:#FD2725">
+                                                <img src="../wp-content/imageedit_1_5405871685.png" style="height:30px" />
+                                            </span>
+                                        </a>
+										</div>
+										<!-- my edit close -->
+
+								
+<!-- about -->
+<div class="about-top" id="about">
+	<div class="container">
+		<h3 class="w3l-title">Logo and Values</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+		
+			
+			
+		
+	</div>
+</div>
+
+					
+				
+				
+
+				
+<!-- //team -->
+<!-- contact -->
+
+			<div class="col-md-3 col-xs-3 wthree_footer_grid_left1">
+				<h4>Navigation</h4>
+				<ul>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="index.html">Home</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#about" class="scroll">About Us</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#services" class="scroll">Services</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#team" class="scroll">Team</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#gallery" class="scroll">Gallery</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#mail" class="scroll">Mail Us</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-xs-3 wthree_footer_grid_left1 w3l-3">
+				<h4>Others</h4>
+				<ul>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#">Media</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#">Mobile Apps</a></li>
+					<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="#">Privacy Policy</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-xs-3 wthree_footer_grid_left1 wthree_footer_grid_right1">
+				<h4>Contact Us</h4>
+				<ul>
+					<li><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailto:info@example.com">info@example.com</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>+(000) 123 311</li>
+					<li><i class="fa fa-fax" aria-hidden="true"></i>+123 421</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<div class="w3layouts_copy_right">
+	<div class="container">
+		<p>© 2017 Scholarly. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts.</a></p>
+	</div>
+</div>
+<!-- //footer -->
+
+<!-- js-scripts -->			
+<!-- js-files -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+<!-- //js-files -->
+<!-- Baneer-js -->
+
+
+
+<!-- smooth scrolling -->
+<script src="js/SmoothScroll.min.js"></script>
+<!-- //smooth scrolling -->
+<!-- stats -->
+<script type="text/javascript" src="js/numscroller-1.0.js"></script>
+<!-- //stats -->
+<!-- moving-top scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //moving-top scrolling -->
+<!-- gallery popup -->
+<script src="js/jquery.swipebox.min.js"></script> 
+<script type="text/javascript">
+jQuery(function($) {
+	$(".swipebox").swipebox();
+});
+</script>
+<!-- //gallery popup -->
+<!--/script-->
+	<script src="js/simplePlayer.js"></script>
+			<script>
+				$("document").ready(function() {
+					$("#video").simplePlayer();
+				});
+			</script>
+<!-- //Baneer-js -->
+<!-- Calendar -->
+<script src="js/jquery-ui.js"></script>
+	<script>
+	  $(function() {
+		$( "#datepicker" ).datepicker();
+	 });
+	</script>
+<!-- //Calendar -->	
+
+<!-- //js-scripts -->
+</body>
+</html>
